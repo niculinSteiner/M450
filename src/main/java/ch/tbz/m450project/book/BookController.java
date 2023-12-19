b66e2b6a314157ef2aa583f2aba825716cce14be
Niculin Steiner <niculin.steiner@ergon.ch>
2023-12-19 10:28:12 +0100

    add cross origin and RestController Annotation

--- src/main/java/ch/tbz/m450project/book/BookController.java
+++ src/main/java/ch/tbz/m450project/book/BookController.java
@@ -1,15 +1,17 @@
 package ch.tbz.m450project.book;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
-@Controller
+@RestController
 @RequestMapping(value = "book")
+@CrossOrigin("*")
 public class BookController {
 
 	BookRepository bookRepository = new BookRepository(new ArrayList<>());
