66a31cca5c97e1b08bf52e04ae1d01ac88968106
Niculin Steiner <niculin.steiner@ergon.ch>
2023-12-19 08:30:44 +0100

    init

---
+++ src/main/java/ch/tbz/m450project/M450ProjectApplication.java
@@ -0,0 +1,13 @@
+package ch.tbz.m450project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M450ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M450ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/tbz/m450project/M450ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.tbz.m450project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M450ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
