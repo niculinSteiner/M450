3ba1343bba616bba36cbe42439af2c12fdeb33c9
Niculin Steiner <niculin.steiner@ergon.ch>
2023-12-19 09:03:25 +0100

    add demo test

--- src/test/java/ch/tbz/m450project/M450ProjectApplicationTests.java
+++ src/test/java/ch/tbz/m450project/M450ProjectApplicationTests.java
@@ -3,11 +3,14 @@ package ch.tbz.m450project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class M450ProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
