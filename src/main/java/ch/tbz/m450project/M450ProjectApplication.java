b8bc76db5410b870d03b6bdf9b920e327d0b5fc4
Niculin Steiner <niculin.steiner@ergon.ch>
2024-01-09 10:51:41 +0100

    Revert "initttt"
    This reverts commit 73562b0cb5ed3e2673dc794e15a958490c798c92.

---
+++ src/main/java/ch/tbz/m450project/M450ProjectApplication.java
@@ -0,0 +1,14 @@
+package ch.tbz.m450project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M450ProjectApplication {
+
+	public static void main(String[] args) {
+		//testst
+		SpringApplication.run(M450ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/tbz/m450project/M450ProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package ch.tbz.m450project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M450ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
